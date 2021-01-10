eaac960d823c9b0c2b97d05fcd9ea0484c630c43
Annette <annetteisgone@gmail.com>
2021-01-10 15:31:13 +0000

    Added buttons and script for HUD

---
+++ git-button-test/Assets/Hud.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+
+    public Text pressedText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PressMe()
+    {
+        Debug.Log("I'm pressed");
+        pressedText.text = "I've been pressed again";
+    }
+
+    public void PressAnother()
+    {
+        Debug.Log("Another is pressed");
+        pressedText.text = "Another's been pressed";
+    }
+}
